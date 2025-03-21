00df15af4bc88e5513d7cd127e2cc28b79ab7e48
Carl Foghammar Nõmtak <c.f.nomtak@gmail.com>
2025-03-21 12:52:39 +0100

    Add FORCE_INLINE

--- pch.h
+++ pch.h
@@ -4,6 +4,20 @@
 #define IMGUI_DEFINE_MATH_OPERATORS
 #endif
 
+#if defined(__clang__)
+#define FORCE_INLINE [[gnu::always_inline]] [[gnu::gnu_inline]] extern inline
+
+#elif defined(__GNUC__)
+#define FORCE_INLINE [[gnu::always_inline]] inline
+
+#elif defined(_MSC_VER)
+#pragma warning(error : 4714)
+#define FORCE_INLINE __forceinline
+
+#else
+#error Unsupported compiler
+#endif
+
 #include "build/_deps/gwca-src/Source/stdafx.h"
 #include <cassert>
 #include <cstdint>
