3bf702b3f3b82bd8d38bf1a066e85a977cbe9ae0
Carl Foghammar Nõmtak <c.f.nomtak@gmail.com>
2025-09-19 20:12:33 +0200

    Make constants constexpr

--- constants.h
+++ constants.h
@@ -15,15 +15,15 @@ namespace Constants
 
     namespace GWColors
     {
-        inline ImU32 damage_yellow = IM_COL32(255, 255, 120, 255);
-        inline ImU32 header_beige = IM_COL32(228, 214, 171, 255);
-        inline ImU32 skill_dynamic_green = IM_COL32(143, 255, 143, 255);
-        inline ImU32 heal_blue = IM_COL32(106, 195, 219, 255);
-        inline ImU32 hp_red = IM_COL32(212, 48, 48, 255);
-        inline ImU32 energy_blue = IM_COL32(65, 150, 215, 255);
-        inline ImU32 elite_gold = IM_COL32(202, 164, 75, 255);
-        inline ImU32 skill_dull_gray = IM_COL32(178, 178, 178, 255);
-        inline ImU32 effect_border_colors[6] = {
+        constexpr inline ImU32 damage_yellow = IM_COL32(255, 255, 120, 255);
+        constexpr inline ImU32 header_beige = IM_COL32(228, 214, 171, 255);
+        constexpr inline ImU32 skill_dynamic_green = IM_COL32(143, 255, 143, 255);
+        constexpr inline ImU32 heal_blue = IM_COL32(106, 195, 219, 255);
+        constexpr inline ImU32 hp_red = IM_COL32(212, 48, 48, 255);
+        constexpr inline ImU32 energy_blue = IM_COL32(65, 150, 215, 255);
+        constexpr inline ImU32 elite_gold = IM_COL32(202, 164, 75, 255);
+        constexpr inline ImU32 skill_dull_gray = IM_COL32(178, 178, 178, 255);
+        constexpr inline ImU32 effect_border_colors[6] = {
             0x00000000, // null
             0xFF46834a, // default green
             0xFF32a8d4, // condition yellow
