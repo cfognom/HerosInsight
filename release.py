49186894605f3e441d35082b1a1f566355baff00
Carl Foghammar Nõmtak <c.f.nomtak@gmail.com>
2026-02-08 14:11:04 +0100

    Push all

--- release.py
+++ release.py
@@ -303,7 +303,7 @@ def public_release(args):
 
     if not has_remote_tag(tag_str):
         print("\nPushing...")
-        subprocess.run(["git", "push", "--follow-tags"], check=True)
+        subprocess.run(["git", "push", "origin", "--follow-tags"], check=True)
 
     try:
         print("\nCreating GitHub release...")
